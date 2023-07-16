f89c77df41e753ac6861af564a2edccdbedca6c5
Aman bhai <rohanrikwar9555@gmail.com>
2023-07-16 23:11:47 +0530

    new_linear-gradient

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-let input=document.querySelectorAll('input')
-let content=document.querySelector('.content')
-let bodyy=document.getElementById('bodyy')
-function gradinet(){
-    let grad=`linear-gradient(to top left,${input[0].value},${input[1].value})`
-   bodyy.style.background=grad
-   content.innerHTML=`background:linear-gradient(to top left,${input[0].value},${input[1].value})`
+let inn=document.querySelectorAll('input');
+let opt=document.getElementById('der')
+let texta=document.getElementById('textaria')
+let container=document.querySelector('.color_container')
+let refr=document.getElementById('Refresh')
+let bo=document.getElementById('bo')
+function leng(randoom){
+    if(randoom){
+       inn[0].value=rencol()
+       inn[1].value=rencol()
+    let gen=` linear-gradient(${opt.value}, ${inn[0].value}, ${inn[1].value})`
+        console.log(rencol())
+        container.style.background=gen
 
+
+    }
+    else{
+        let gen=` linear-gradient(${opt.value}, ${inn[0].value}, ${inn[1].value})`
+        container.style.background=gen
+        texta.value=`background:${gen}`
+    }
+    //let gen=` linear-gradient(${opt.value}, ${inn[0].value}, ${inn[1].value})`
+    //container.style.background=gen
+   // texta.value=`background:${gen}`
 }
-input.forEach(input=>{
-input.addEventListener('input',gradinet)
+inn.forEach(inp=>{
+    inp.addEventListener('input',leng)
 })
+function rencol(){
+    let renc=Math.floor(Math.random()*0xffffff).toString(16)
+    return `#${renc}`
+}
+
+opt.addEventListener("change",leng) 
+refr.addEventListener('click',()=>{leng(true)})
